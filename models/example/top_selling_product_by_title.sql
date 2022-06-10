3511ae8b2381943f07781aed6c220a27999bbd94
DevMaurice <developermaurice@gmail.com>
2022-06-10 15:41:14 +0300

    fix:Add date filter

--- models/example/top_selling_product_by_title.sql
+++ models/example/top_selling_product_by_title.sql
@@ -1,9 +1,9 @@
 
 {{ config(materialized='table') }}
 
-
 select title, created_at,sum(price) as total 
 from {{ ref("cleaned_order_line")}}
+where year(created_at)=2022
 group by title,created_at
 order by total desc
 
